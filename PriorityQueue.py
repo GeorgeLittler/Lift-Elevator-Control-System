c010bed5df82a6630a20dc850badb6241ecd42f6
laraeksi <necladerinlaraeksi@gmail.com>
2025-02-05 20:59:23 +0000

    planed_priorityqueue

---
+++ PriorityQueue.py
@@ -0,0 +1,10 @@
+#function loading priority queue the requests(Store 2 element of the request list differently )
+
+#specify the priority and be careful on u need to (when a person at five make a reqpuest to 0 and there is person at second floor (first question:should we allow her to enter the elevator:YES, second question:So if yes and she clicked floor 4 would elevator go floor 0 then floor 4:Yes=changed to no,
+# so when a person is making request they are not making request of the direction(if they did, lift would not let her to lift before arriving to floor 0):NO )
+
+#function removing element from priority queue
+
+#The prority is still first in however other priority is it is on the way 
+#or add in removing function this:checking the priority and remove it like that (I think this is how actually priority queue logic)
+
