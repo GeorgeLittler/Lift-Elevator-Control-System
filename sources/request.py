e6e33173e93f1341b8b6417834080f60399fbb4f
laraeksi <necladerinlaraeksi@gmail.com>
2025-02-05 21:14:28 +0000

    created request class with destination floor

--- sources/request.py
+++ sources/request.py
@@ -1,4 +1,5 @@
+#This object represents the request of the passenger, the floor they start at and the floor they're going to
 class Request:
-    def __init__(self, start_floor, destination_direction):
+    def __init__(self, start_floor, destination_floor):
         self.start_floor = start_floor
-        self.destination_direction = destination_direction
+        self.destination_floor = destination_floor
