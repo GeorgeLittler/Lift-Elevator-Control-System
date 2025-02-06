a088d459cc42654b0357e8bdc9c62db7152ff1ae
laraeksi <necladerinlaraeksi@gmail.com>
2025-02-06 16:54:43 +0000

    priority_queue is planned totally

--- sources/priority_queue.py
+++ sources/priority_queue.py
@@ -1,10 +1,62 @@
-#function loading priority queue the requests(Store 2 element of the request list differently )
+#function loading request into priority queues (Active and waiting to limit the people get on the queue)
+#check if the active queue is equal to capasity limit or not if not full continue
+    #if they are going the direction add them into the active 
+    #elseif they are going opposite put them in waiting 
+#else if the active queue is full put them in waiting 
 
-#specify the priority and be careful on u need to (when a person at five make a reqpuest to 0 and there is person at second floor (first question:should we allow her to enter the elevator:YES, second question:So if yes and she clicked floor 4 would elevator go floor 0 then floor 4:Yes=changed to no,
-# so when a person is making request they are not making request of the direction(if they did, lift would not let her to lift before arriving to floor 0):NO )
 
-#function removing element from priority queue
+#function loading waitingQueue to activeQueue 
+#check if the active queue is equal to capasity limit if not
+    #ADDING SOMETHING
+    #load the element[0] of the waiting queue to the active queue element[n] 
+    # and delete it from waiting queue  
 
-#The prority is still first in however other priority is it is on the way 
-#or add in removing function this:checking the priority and remove it like that (I think this is how actually priority queue logic)
+#Function Minheap(ActiveQueue)
+# if direction positive in all requests in lift continue
+    #Loop the all elements(requests) in active Queue
+        #Then take the both elements of requests and store it in list
+    #return that list and put it in sortingBubble function 
+    #function sorting bubble_for minheap(the list)
+    #sort the order by bubble sort meaning get every element and compare it with every element in list
+        #loop through each element 
+            #if element[n] > element[n+1]
+                #store value of element[n] in the place element[n+1]
+                #call function sorting bubble
+            #elseif element[n] == element[n+1]
+                #get rid of one n+1 by moving every request at the left move one right
+                #by using "del list[n+1]
+            #else don't do anything(don't add this line)
+    #return the MinHeap queue
+#else return Error
+
+#Function Maxheap(ActiveQueue)
+# if direction negative in all requests in lift continue
+    #Loop the all elements(requests) in active Queue
+        #Then take the both element of requests and store it in list
+    #return that list and put it in sortingBubble function 
+    #function sorting bubble_for maxheap(the list)
+    #sort the order by bubble sort meaning get every element and compare it with every element in list
+        #loop through each element 
+            #if element[n] < element[n+1]
+                #store value of element[n] in the place element[n+1]
+                #call function sorting bubble
+            #elseif element[n] == element[n+1]
+                #get rid of one n+1 by moving every request at the left move one right
+                #by using "del list[n+1]
+            #else don't do anything(don't add this line)
+    #return the MaxHeap queue
+#else return Error
+
+
+#function removing element from queues(Active Queue,Queue(an input of Max or MinHeap))
+    #loop i the number of elements in Queue(Min/Heap)
+        #if current.place.lift==Queue[i]
+            #del Queue[i]
+            #Loop(k) the requests at ActiveQueue 
+                #if ActiveQueue[k] (a request) is [VISITED,VISITED]
+                    #del ActiveQueue[k]
+                #loop (j) the elements in request
+                    #if request[j]==current.place.lift
+                        #then put VISITED in the place request[j] 
+
 
