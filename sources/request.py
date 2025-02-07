a19cc7de4374b5ae30ba8156ad284bad4bcc8822
laraeksi <necladerinlaraeksi@gmail.com>
2025-02-07 16:05:11 +0000

    priority_queue is done but without defined Lift capasity and current place of the lift

--- sources/request.py
+++ sources/request.py
@@ -3,3 +3,10 @@ class Request:
     def __init__(self, start_floor, destination_floor):
         self.start_floor = start_floor
         self.destination_floor = destination_floor
+    def request_direction(self):
+        if self.start_floor>self.destination_floor:
+            direction1="negative"
+            return direction1
+        if self.start_floor<self.destination_floor:
+            direction1="positive"
+            return direction1
